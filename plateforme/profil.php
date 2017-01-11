872b85b9e8209f5a25891fded7194a286a2d6a86
Kathy <gremillet.kathy@gmail.com>
2017-01-11 17:26:46 +0100

    fusion des 2 fichiers profil suite au conflit

--- plateforme/profil.php
+++ plateforme/profil.php
@@ -12,54 +12,57 @@
 		$cheminCV = "../membres/cv/".$userInfo['cv'];
 		$cheminLM = "../membres/lettreMotivation/".$userInfo['lm'];
 
+
+	$page = "Profil";
+    $description = "Avenir - La plateforme de la réoriention facile - Page profil";
+
+    include('../includes/headerPlateforme.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Profil</title>
-</head>
-<body>
+	<div class=" profil" id="main-content">
+		
+		<?php include('../includes/secondNav.php'); ?>
 
-<div align="center">
-	<h2>Profil de <?php echo $userInfo['prenom'].' '.$userInfo['nom'] ; ?></h2>
-	<br><br>
-	<?php
-		if(!empty($userInfo['avatar'])) { ?>
+		<div align="center">
+			<h2>Profil de <?php echo $userInfo['prenom'].' '.$userInfo['nom'] ; ?></h2>
+			<br><br>
+			<?php
+				if(!empty($userInfo['avatar'])) { ?>
 
-		<img src="../membres/avatar/<?php echo $userInfo['avatar']; ?>" alt="">
+				<img src="../membres/avatar/<?php echo $userInfo['avatar']; ?>" alt="">
 
-	<?php }
-	?> <br>
-	Prenom = <?php echo $userInfo['prenom']; ?>
-	<br>
-	Nom = <?php echo $userInfo['nom']; ?>
-	<br>
-	Mail = <?php echo $userInfo['mail']; ?>
-	<br>
-	Âge = <?php echo $userInfo['age']; ?>
-	<br>
-	Adresse = <?php if(isset($userInfo['adresse'])) { echo $userInfo['adresse']; } else { echo "Vous n'avez pas encore rempli votre adresse"; } ?>
-	<br>
-	Adresse (suite) = <?php if(isset($userInfo['adresse2'])) { echo $userInfo['adresse2']; } else { echo "Vous n'avez pas encore rempli votre adresse (suite)"; } ?>
-	<br>
-	Code postal = <?php if(isset($userInfo['cp'])) { echo $userInfo['cp']; } else { echo "Vous n'avez pas encore rempli votre code postal"; } ?>
-	<br>
-	Ville = <?php if(isset($userInfo['ville'])) { echo $userInfo['ville']; } else { echo "Vous n'avez pas encore rempli votre ville"; } ?>
-	<br>
-	CV = <?php if(isset($userInfo['cv'])) { echo '<a target="_blank" href="'.$cheminCV.'" onclick="window.open(this.href, \'Popup\', \'scrollbars=1,resizable=1,height=560,width=770\'); return false;">'.$userInfo['cv'].'</a>'; } else { echo "Vous n'avez pas encore attaché de CV"; } ?>
-	<br>
-	Lettre de motivation = <?php if(isset($userInfo['lm'])) { echo '<a target="_blank" href="'.$cheminLM.'" onclick="window.open(this.href, \'Popup\', \'scrollbars=1,resizable=1,height=560,width=770\'); return false;">'.$userInfo['lm'].'</a>'; } else { echo "Vous n'avez pas encore attaché de lettre de motivation"; } ?>
-	<br>
-	<?php
-	if(isset($_SESSION['id']) AND $userInfo['id'] == $_SESSION['id']) { ?>
-		<a href="editionProfil.php">Editer mon profil</a>
-		<a href='../index.php'>Retourner sur l'accueil</a>
-		<a href="deconnexion.php">Se déconnecter</a>
-	<?php } ?>
+			<?php }
+			?> <br>
+			Prenom = <?php echo $userInfo['prenom']; ?>
+			<br>
+			Nom = <?php echo $userInfo['nom']; ?>
+			<br>
+			Mail = <?php echo $userInfo['mail']; ?>
+			<br>
+			Âge = <?php echo $userInfo['age']; ?>
+			<br>
+			Adresse = <?php if(isset($userInfo['adresse'])) { echo $userInfo['adresse']; } else { echo "Vous n'avez pas encore rempli votre adresse"; } ?>
+			<br>
+			Adresse (suite) = <?php if(isset($userInfo['adresse2'])) { echo $userInfo['adresse2']; } else { echo "Vous n'avez pas encore rempli votre adresse (suite)"; } ?>
+			<br>
+			Code postal = <?php if(isset($userInfo['cp'])) { echo $userInfo['cp']; } else { echo "Vous n'avez pas encore rempli votre code postal"; } ?>
+			<br>
+			Ville = <?php if(isset($userInfo['ville'])) { echo $userInfo['ville']; } else { echo "Vous n'avez pas encore rempli votre ville"; } ?>
+			<br>
+			CV = <?php if(isset($userInfo['cv'])) { echo '<a target="_blank" href="'.$cheminCV.'" onclick="window.open(this.href, \'Popup\', \'scrollbars=1,resizable=1,height=560,width=770\'); return false;">'.$userInfo['cv'].'</a>'; } else { echo "Vous n'avez pas encore attaché de CV"; } ?>
+			<br>
+			Lettre de motivation = <?php if(isset($userInfo['lm'])) { echo '<a target="_blank" href="'.$cheminLM.'" onclick="window.open(this.href, \'Popup\', \'scrollbars=1,resizable=1,height=560,width=770\'); return false;">'.$userInfo['lm'].'</a>'; } else { echo "Vous n'avez pas encore attaché de lettre de motivation"; } ?>
+			<br>
+			<?php
+			if(isset($_SESSION['id']) AND $userInfo['id'] == $_SESSION['id']) { ?>
+				<a href="editionProfil.php">Editer mon profil</a>
+				<a href='../index.php'>Retourner sur l'accueil</a>
+				<a href="deconnexion.php">Se déconnecter</a>
+			<?php } ?>
 
-</div>
+		</div>
+
+	</div>
 	
 </body>
 </html>
