0d7bf68d283b4213d55fb69773fcdedbdca6ab77
Kathy <gremillet.kathy@gmail.com>
2017-01-12 12:07:04 +0100

    ajout du tchatbox + suppression fichier test + gestion avatar

--- includes/headerPlateforme.php
+++ includes/headerPlateforme.php
@@ -7,18 +7,10 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<!-- Latest compiled and minified CSS -->
-	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
-
-	<!-- Optional theme -->
-	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">-->
-
-	<!-- Latest compiled and minified JavaScript -->
-	<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>-->
-
 	<link rel="stylesheet" type="text/css" href="../css/global.css" />		
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script type="text/javascript" src="../js/production.min.js"></script>
+	<script type="text/javascript">$crisp=[];CRISP_WEBSITE_ID="1d13b762-89c4-4f62-a9f5-2ecc98acdc4a";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
 </head>
 
 <body class="">
--- plateforme/accueil.php
+++ plateforme/accueil.php
@@ -4,7 +4,7 @@
 
 	if(isset($_SESSION['id'])){
 
-		$reqUser = $bdd->prepare('SELECT * FROM user WHERE id = ?');
+		$reqUser = $bdd->prepare('SELECT * FROM user, profil WHERE id = ?');
 		$reqUser->execute(array($_SESSION['id']));
 		$userInfo = $reqUser->fetch();
 
