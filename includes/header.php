bab2a368d54e56691c0e26705869e5e5c356ae02
MaevaAoigami <ridardmaeva@gmail.com>
2017-01-12 16:05:10 +0100

    Correctifs

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
 
 		<link rel="stylesheet" type="text/css" href="css/global.css" />		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>	
-		<script type="text/javascript" src="./js/production.min.js"></script>
+		<script type="text/javascript" src="js/production.min.js"></script>
 	</head>
 
 	<body>
