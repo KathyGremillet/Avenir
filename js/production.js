c8a85061912be9e0777f42d6e08f357b1f826791
Kathy <gremillet.kathy@gmail.com>
2017-01-17 00:01:45 +0100

    Integration profil

--- js/production.js
+++ js/production.js
@@ -1,5 +1,6 @@
 $(function(){
 
 	$('.bloc-news').find('div').matchHeight();
+	$('.presentation').children('div').matchHeight();
 
 });
--- plateforme/profil.php
+++ plateforme/profil.php
@@ -19,48 +19,119 @@
     include('../includes/headerPlateforme.php');
 ?>
 
-	<div class=" profil" id="main-content">
+	<div class="" id="main-content">
 		
 		<?php include('../includes/secondNav.php'); ?>
 
-		<div align="center">
-			<h2>Profil de <?php echo $userInfo['prenom'].' '.$userInfo['nom'] ; ?></h2>
-			<br><br>
-			<?php
-				if(!empty($userInfo['avatar'])) { ?>
+		<div class="profil">
+			<div class="presentation row">
+				<div class=" col-lg-8">
+					<div class="blocs">
+						<div class="clearfix">
+							<div class="avatar col-lg-4">
+								<?php
+									if(!empty($userInfo['avatar'])) {
 
-				<img src="../membres/avatar/<?php echo $userInfo['avatar']; ?>" alt="">
-
-			<?php }
-			?> <br>
-			Prenom = <?php echo $userInfo['prenom']; ?>
-			<br>
-			Nom = <?php echo $userInfo['nom']; ?>
-			<br>
-			Mail = <?php echo $userInfo['mail']; ?>
-			<br>
-			Âge = <?php if(isset($userInfo['age'])) { echo $userInfo['age']; } else { echo "Vous n'avez pas encore rempli votre âge"; } ?>
-			<br>
-			Adresse = <?php if(isset($userInfo['adresse'])) { echo $userInfo['adresse']; } else { echo "Vous n'avez pas encore rempli votre adresse"; } ?>
-			<br>
-			Adresse (suite) = <?php if(isset($userInfo['adresse2'])) { echo $userInfo['adresse2']; } else { echo "Vous n'avez pas encore rempli votre adresse (suite)"; } ?>
-			<br>
-			Code postal = <?php if(isset($userInfo['cp'])) { echo $userInfo['cp']; } else { echo "Vous n'avez pas encore rempli votre code postal"; } ?>
-			<br>
-			Ville = <?php if(isset($userInfo['ville'])) { echo $userInfo['ville']; } else { echo "Vous n'avez pas encore rempli votre ville"; } ?>
-			<br>
-			CV = <?php if(isset($userInfo['cv'])) { echo '<a target="_blank" href="'.$cheminCV.'" onclick="window.open(this.href, \'Popup\', \'scrollbars=1,resizable=1,height=560,width=770\'); return false;">'.$userInfo['cv'].'</a>'; } else { echo "Vous n'avez pas encore attaché de CV"; } ?>
-			<br>
-			Lettre de motivation = <?php if(isset($userInfo['lm'])) { echo '<a target="_blank" href="'.$cheminLM.'" onclick="window.open(this.href, \'Popup\', \'scrollbars=1,resizable=1,height=560,width=770\'); return false;">'.$userInfo['lm'].'</a>'; } else { echo "Vous n'avez pas encore attaché de lettre de motivation"; } ?>
-			<br>
-			<?php
-			if(isset($_SESSION['id']) AND $userInfo['id'] == $_SESSION['id']) { ?>
-				<a href="editionProfil.php">Editer mon profil</a>
-				<a href='accueil.php'>Retourner sur l'accueil</a>
-				<a href="deconnexion.php">Se déconnecter</a>
-			<?php } ?>
-
-		</div>
+									echo '<img src="../membres/avatar/' . $userInfo['avatar'] . '" alt="Photo de profil">';
+								
+									} else{ 
+										echo '<img src="../membres/avatar/default.jpg" alt="Photo de profil par défaut">';
+									}
+								?>
+							</div>	
+							<div class="infos col-lg-8">
+								<h2><?php echo $userInfo['prenom'] . ' ' . $userInfo['nom']; ?></h2>
+								<div class="coordonnees row">
+									<div class="col-lg-3">
+										<span class="typcn typcn-location"></span> <?php if(isset($userInfo['ville'])) { echo $userInfo['ville']; } ?>
+									</div>
+									<div class="col-lg-5">
+										<span class="typcn typcn-device-phone"></span> 06 00 00 00 00
+									</div>
+									<div class="col-lg-4">
+										<span class="typcn typcn-mail"></span> <?php echo $userInfo['mail']; ?>
+									</div>
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+								</p>						
+							</div>
+						</div>
+						
+						<span class="typcn typcn-edit"></span>
+					</div>					
+				</div>
+				<div class="cv col-lg-2">
+					<div>
+						<img src="../images/profil-cv.png" alt="Icône CV">
+						<p>CV</p>
+						<span class="typcn typcn-download-outline"></span>
+					</div>
+				</div>
+				<div class="lm col-lg-2">
+					<div>
+						<img src="../images/profil-LettreDeMotivation.png" alt="Icône Lettre de motivation">
+						<p>Lettre de motivation</p>
+						<span class="typcn typcn-download-outline"></span>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="exp-pro col-lg-6">
+					<h3><span class="typcn typcn-briefcase"> Expérience professionnelle</h3>
+					<div class="blocs">
+						<div>
+							<h4>Poste</h4>
+							<h5>Entreprise</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+						</div>
+						<div>
+							<h4>Poste</h4>
+							<h5>Entreprise</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+						</div>
+						<span class="typcn typcn-edit"></span>
+					</div>
+				</div>
+				<div class="cursus col-lg-6">
+					<h3><span class="typcn typcn-mortar-board"> Cursus scolaire</h3>
+					<div class="blocs">
+						<div>
+							<h4>Formation</h4>
+							<h5>École</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+						</div>
+						<div>
+							<h4>Formation</h4>
+							<h5>École</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+						</div>
+						<span class="typcn typcn-edit"></span>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="competences col-lg-6">
+					<h3><span class="typcn typcn-puzzle-outline"></span> Compétences</h3>
+					<ul>
+						<li>Sens du contact</li>
+						<li>Résistance au stress</li>
+						<li>Spécialisation et technicité</li>
+					</ul>
+					<span class="typcn typcn-edit"></span>
+				</div>
+				<div class="ctr-interets col-lg-6">
+					<h3><span class="typcn typcn-heart-outline"></span> Centres d'intérêts</h3>
+					<ul>
+						<li>Running</li>
+						<li>Dessin</li>
+						<li>Partir à l'aventure</li>
+						<li>Manager</li>
+					</ul>
+					<span class="typcn typcn-edit"></span>
+				</div>
+			</div>
+		</div>	
 
 	</div>
 	
